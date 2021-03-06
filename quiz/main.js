6d8b55c72dc1604218c7df362246c5c8de76ef5e
Semen <sukhorada.s.a@gmail.com>
2021-03-06 20:11:32 +0300

    chapter 4

--- quiz/main.js
+++ quiz/main.js
@@ -1,22 +1,17 @@
-document.addEventListener('readystatechange', () => {
-    if (document.readyState === 'complete') setTimeout(main,100);
-})
-
-const main = () => {
-    const quiz = [
-        ["What is Superman's real name?", "Clark Kent"],
-        ["What is Wonder Woman's real name?", "Diana Prince"],
-        ["What is Batman's real name?", "Bruce Wayne"]
-    ];
-    const question = "What is Superman's real name?";
-    const answer = prompt(question);
-
+function start(quiz) {
     let score = 0;
-
-    alert(`You answered ${answer}`);
-
+    // main game loop
     for (const [question, answer] of quiz) {
-        const response = prompt(question);
+        const response = ask(question);
+        check(response, answer);
+    }
+    // end of main game loop
+    gameOver();
+    function ask(question) {
+        return prompt(question);
+    }
+
+    function check(response, answer) {
         if (response === answer) {
             alert('Correct!');
             score++;
@@ -24,6 +19,22 @@ const main = () => {
             alert(`Wrong! The correct answer was ${answer}`);
         }
     }
-    // At the end of the game, report the player's score
-    alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+
+    function gameOver() {
+        // At the end of the game, report the player's score
+        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+    }
 }
+
+const main = () => {
+    const quiz = [
+        ["What is Superman's real name?", "Clark Kent"],
+        ["What is Wonder Woman's real name?", "Diana Prince"],
+        ["What is Batman's real name?", "Bruce Wayne"]
+    ];
+    start(quiz);
+}
+
+document.addEventListener('readystatechange', () => {
+    if (document.readyState === 'complete') setTimeout(main, 100);
+})
