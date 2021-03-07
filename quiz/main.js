b12913f5dba3479cc1dd12ff1741f6b5a28a9e96
Semen <sukhorada.s.a@gmail.com>
2021-03-07 16:23:52 +0300

    chapter 5 end

--- quiz/main.js
+++ quiz/main.js
@@ -1,38 +1,42 @@
-function start(quiz) {
-    let score = 0;
-    // main game loop
-    for (const [question, answer] of quiz) {
-        const response = ask(question);
-        check(response, answer);
-    }
-    // end of main game loop
-    gameOver();
-    function ask(question) {
-        return prompt(question);
-    }
+const game = {
+    start(quiz) {
+        this.questions = [...quiz];
+        this.score = 0;
+        // main game loop
+        for (const question of this.questions) {
+            this.question = question;
+            this.ask();
+        }
+        // end of main game loop
+        this.gameOver();
+    },
+    ask() {
+        const question = `What is ${this.question.name}'s real name?`;
+        const response = prompt(question);
+        this.check(response);
+    },
 
-    function check(response, answer) {
+    check(response) {
+        const answer = this.question.realName;
         if (response === answer) {
             alert('Correct!');
-            score++;
+            this.score++;
         } else {
             alert(`Wrong! The correct answer was ${answer}`);
         }
-    }
-
-    function gameOver() {
-        // At the end of the game, report the player's score
-        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+    },
+    gameOver() {
+        alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
     }
 }
 
 const main = () => {
     const quiz = [
-        ["What is Superman's real name?", "Clark Kent"],
-        ["What is Wonder Woman's real name?", "Diana Prince"],
-        ["What is Batman's real name?", "Bruce Wayne"]
+        { name: "Superman", realName: "Clark Kent" },
+        { name: "Wonder Woman", realName: "Diana Prince" },
+        { name: "Batman", realName: "Bruce Wayne" },
     ];
-    start(quiz);
+    game.start(quiz);
 }
 
 document.addEventListener('readystatechange', () => {
