09fcfc411b9f1c849cede1bbf383caa51d3cefdf
Semen <sukhorada.s.a@gmail.com>
2021-03-07 22:46:43 +0300

    chapter 6

--- quiz/main.js
+++ quiz/main.js
@@ -12,6 +12,7 @@ const game = {
     },
     ask() {
         const question = `What is ${this.question.name}'s real name?`;
+        view.render(view.question,question);
         const response = prompt(question);
         this.check(response);
     },
@@ -19,23 +20,41 @@ const game = {
     check(response) {
         const answer = this.question.realName;
         if (response === answer) {
+            view.render(view.result,'Correct!',{'class':'correct'});
             alert('Correct!');
             this.score++;
+            view.render(view.score,this.score);
         } else {
+            view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
             alert(`Wrong! The correct answer was ${answer}`);
         }
     },
     gameOver() {
-        alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+        view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
     }
 }
 
+const quiz = [
+    { name: "Superman", realName: "Clark Kent" },
+    { name: "Wonder Woman", realName: "Diana Prince" },
+    { name: "Batman", realName: "Bruce Wayne" },
+]; 
+
+// View Object
+const view = {
+    score: document.querySelector('#score strong'),
+    question: document.getElementById('question'),
+    result: document.getElementById('result'),
+    info: document.getElementById('info'),
+    render(target, content, attributes) {
+        for (const key in attributes) {
+            target.setAttribute(key, attributes[key]);
+        }
+        target.innerHTML = content;
+    }
+};
+
 const main = () => {
-    const quiz = [
-        { name: "Superman", realName: "Clark Kent" },
-        { name: "Wonder Woman", realName: "Diana Prince" },
-        { name: "Batman", realName: "Bruce Wayne" },
-    ];
     game.start(quiz);
 }
 
